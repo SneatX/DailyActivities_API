68d5d00f9865228e81cb4bce31be6b1ba9a05f97
SneatX <santi02005@hotmail.com>
2024-10-23 22:53:19 -0500

    feat: :sparkles: Creating the api with express

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-import UsersModel from './src/models/usersModel.js';
+import express from 'express'
+import cors from 'cors'
+
 process.loadEnvFile()
 
-const usersModel = new UsersModel();
-usersModel.getAll()
+const app = express()
+app.use(cors())
+
+const port = process.env.EXPRESS_PORT || 3000
+
+app.listen(port, () => {
+    console.log('http://localhost:' + port)
+})
+
+app.use("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+export default app
